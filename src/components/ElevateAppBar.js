5ab1204ed816c4a9116a3446007de58cfbf3a060
Mehrdad Mehralian <dadwic@yahoo.com>
2020-11-29 00:21:03 +0330

    feat: mui appbar and parallax header added

---
+++ src/components/ElevateAppBar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+const useStyles = makeStyles((theme) => ({
+  brand: {
+    fontFamily: 'monospace',
+  },
+}));
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 1 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ElevateAppBar(props) {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <ElevationScroll {...props}>
+        <AppBar color="default">
+          <Toolbar>
+            <Typography variant="h6" className={classes.brand}>
+              david.sh
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar id="back-to-top-anchor" />
+    </React.Fragment>
+  );
+}
